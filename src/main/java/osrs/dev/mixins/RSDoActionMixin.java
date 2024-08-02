e2ddbcecedfd90bf8b4a27468dbded477d2f336c
TonicBox <gsec.tonicbox@protonmail.com>
2024-08-02 15:52:23 -0500

    Little cleanup.

--- src/main/java/osrs/dev/mixins/RSDoActionMixin.java
+++ src/main/java/osrs/dev/mixins/RSDoActionMixin.java
@@ -19,32 +19,8 @@ public abstract class RSDoActionMixin
     @MethodHook("menuAction")
     public static boolean doAction(int param0, int param1, int opcode, int identifier, int itemId, int worldViewId, String option, String target, int canvasX, int canvasY)
     {
-//        StringBuilder log = new StringBuilder();
-//        log.append("[MA] param0=")
-//                .append(param0)
-//                .append(", param1=")
-//                .append(param1)
-//                .append(", opcode=")
-//                .append(opcode)
-//
-//                .append(", identifier=")
-//                .append(identifier)
-//                .append(", itemId=")
-//                .append(itemId)
-//                .append(", worldViewId=")
-//                .append(worldViewId)
-//                .append(", option=")
-//                .append(option)
-//                .append(", target=")
-//                .append(target)
-//                .append(", canvasX=")
-//                .append(canvasX)
-//                .append(", canvasY=")
-//                .append(canvasY);
         MenuOptionClicked event = new MenuOptionClicked(param0, param1, opcode, identifier, itemId, worldViewId, option, target, canvasX, canvasY);
-
         EventBus.post(getClient(), event);
-        System.out.println("posted!");
         return false;
     }
 }
